175d3332eef0b1dd6786faa28a9a87868fd1421f
Pradip Caulagi <caulagi@gmail.com>
2017-06-25 00:23:10 +0200

    Migrate some existing tests to postgres

--- test/test_slogan_manager.py
+++ test/test_slogan_manager.py
@@ -1,59 +1,80 @@
 """
 Tests for interaction with db for slogans
 """
-import sqlite3
-from tempfile import NamedTemporaryFile
+import asyncio
+import random
+import string
 from unittest import TestCase
 
+import asyncpg
+
+from server.const import connection_url
 from server.slogan_manager import SloganManager
 
 
 class SloganManagerTest(TestCase):
+
+    @staticmethod
+    def random_title():
+        return ''.join(random.choice(string.ascii_lowercase) for i in range(20))
+
+    @classmethod
+    def setUpClass(cls):
+        cls.sm = SloganManager()
+        cls.loop = asyncio.get_event_loop()
+
+    async def _test_init(self):
+        await self.sm.init()
+        conn = await asyncpg.connect(connection_url())
+        row = await conn.fetchrow(
+            'select table_name from information_schema.tables where table_name = \'slogan\''
+        )
+        assert row['table_name'] == 'slogan'
+
     def test_init(self):
-        with NamedTemporaryFile() as test_db:
-            SloganManager(test_db.name)
-            with sqlite3.connect(test_db.name) as db:
-                cursor = db.cursor()
-                cursor.execute('''
-                    select name from sqlite_master where type = 'table' and name = 'slogan'
-                ''')
-                assert cursor.fetchone()[0] == 'slogan'
+        self.loop.run_until_complete(self._test_init())
 
     def test_md5(self):
-        assert SloganManager.get_md5(
-            'test') == '098f6bcd4621d373cade4e832627b4f6'
+        assert SloganManager.get_md5('test') == '098f6bcd4621d373cade4e832627b4f6'
+
+    async def _test_create(self):
+        title = self.random_title()
+        ok, res = await self.sm.create(title)
+        assert ok is True
+        assert res == title
 
     def test_create(self):
-        with NamedTemporaryFile() as test_db:
-            slogan_manager = SloganManager(test_db.name)
-            assert slogan_manager.create('test')[1] == 'test'
+        self.loop.run_until_complete(self._test_create())
+
+    async def _test_create_unique_constraint(self):
+        title = self.random_title()
+        await self.sm.create(title)
+        ok, _ = await self.sm.create(title)
+        assert ok is False
 
     def test_create_unique_constraint(self):
-        with NamedTemporaryFile() as test_db:
-            slogan_manager = SloganManager(test_db.name)
-            slogan_manager.create('test')
-            with self.assertRaises(sqlite3.IntegrityError):
-                slogan_manager.create('test')[1]
+        self.loop.run_until_complete(self._test_create_unique_constraint())
+
+    async def _test_rent_when_available(self):
+        title = self.random_title()
+        await self.sm.create(title)
+        status, _ = await self.sm.rent(rented_by=title)
+        assert status is True
 
     def test_rent_when_available(self):
-        with NamedTemporaryFile() as test_db:
-            slogan_manager = SloganManager(test_db.name)
-            slogan_manager.create('test')
-            status, title = slogan_manager.rent()
-            assert status
-            assert title == 'test'
+        self.loop.run_until_complete(self._test_rent_when_available())
 
-    def test_rent_none_available(self):
-        with NamedTemporaryFile() as test_db:
-            slogan_manager = SloganManager(test_db.name)
-            slogan_manager.create('test')
-            slogan_manager.rent()
-            status, _ = slogan_manager.rent()
-            assert status is False
+    # def test_rent_none_available(self):
+    #     with NamedTemporaryFile() as test_db:
+    #         slogan_manager = SloganManager(test_db.name)
+    #         slogan_manager.create('test')
+    #         slogan_manager.rent()
+    #         status, _ = slogan_manager.rent()
+    #         assert status is False
 
-    def test_list(self):
-        with NamedTemporaryFile() as test_db:
-            slogan_manager = SloganManager(test_db.name)
-            slogan_manager.create('test 1')
-            slogan_manager.create('test 2')
-            assert len(slogan_manager.list()) == 2
+    # def test_list(self):
+    #     with NamedTemporaryFile() as test_db:
+    #         slogan_manager = SloganManager(test_db.name)
+    #         slogan_manager.create('test 1')
+    #         slogan_manager.create('test 2')
+    #         assert len(slogan_manager.list()) == 2
