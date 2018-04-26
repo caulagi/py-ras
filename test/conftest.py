5dbce0f2218d211b9c4a89cab2e862a6d3903377
Pradip Caulagi <caulagi@gmail.com>
2018-04-26 20:22:50 +0200

    Can't drop database when we are connected to it

--- test/conftest.py
+++ test/conftest.py
@@ -9,11 +9,11 @@ def pytest_configure():
     os.environ['PG_DATABASE'] = 'slogan_test'
 
     from server.client_manager import ClientManager
-    from server.const import connection_url
+    from server.const import PG_USERNAME, PG_PASSWORD
     from server.slogan_manager import SloganManager
 
     async def init_db():
-        conn = await asyncpg.connect(connection_url())
+        conn = await asyncpg.connect(user=PG_USERNAME, password=PG_PASSWORD)
         await conn.execute('DROP DATABASE IF EXISTS %s' % os.environ['PG_DATABASE'])
         await conn.execute('CREATE DATABASE slogan_test')
         await SloganManager().init()
