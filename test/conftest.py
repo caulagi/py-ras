bc3988b040352881c2e47b7143ba9db8801a8a15
Pradip Caulagi <caulagi@gmail.com>
2018-04-26 23:56:16 +0530

    Tidy tests (#8)
    Tests should not rely on database being created and environment set

--- test/conftest.py
+++ test/conftest.py
@@ -1,16 +1,21 @@
 import asyncio
 import os
 
+import asyncpg
+
 
 def pytest_configure():
     # environment should be set before importing
-    os.environ['PG_DATABASE'] = os.environ.get('PG_DATABASE', 'slogan_test')
-    os.environ['PG_PASSWORD'] = os.environ.get('PG_PASSWORD', '')
+    os.environ['PG_DATABASE'] = 'slogan_test'
 
     from server.client_manager import ClientManager
+    from server.const import PG_USERNAME, PG_PASSWORD
     from server.slogan_manager import SloganManager
 
     async def init_db():
+        conn = await asyncpg.connect(user=PG_USERNAME, password=PG_PASSWORD)
+        await conn.execute('DROP DATABASE IF EXISTS %s' % os.environ['PG_DATABASE'])
+        await conn.execute('CREATE DATABASE slogan_test')
         await SloganManager().init()
         await ClientManager().init()
 
